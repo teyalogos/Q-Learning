766bc85f1a67ca358fd2154a50b8c41ed6a36e75
Mateo Juan A. Llanes <microbestyles@gmail.com>
2017-12-07 10:57:28 +0800

    Update level.py

--- level.py
+++ level.py
@@ -19,7 +19,7 @@ class environment(object):
 
 
 
-    def __init__(self, board_size=5, goal_x=[4], goal_y=[4],
+    def __init__(self, board_size=5, goal_x=[], goal_y=[],
                  player_x=0, player_y=0, goal_num=1):
         'Initialize Object'
 
